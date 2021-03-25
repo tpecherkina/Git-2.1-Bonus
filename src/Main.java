1deb02b6c0bf514478bdde0d99c59996b6bae64d
tpecherkina <tpecherkina18@gmail.com>
2021-03-24 22:06:26 -0400

    Init

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        long balance = 100;
+        long amountOfDeposit = 2000;
+        if (amountOfDeposit > 1000);
+        balance += amountOfDeposit*0.01+amountOfDeposit;
+        System.out.println(balance);
+    }
+}
