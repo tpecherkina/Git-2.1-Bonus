e1bfffc6610381fe9cad14b95a5e95019c83538e
tpecherkina <74115313+tpecherkina@users.noreply.github.com>
2021-03-25 21:30:23 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,10 @@ public class Main {
     public static void deal(int amountOfDeposit) {
         int initialBalance=100;
         int finalBalance;
-        int amountOfDeposit;
-        if (amountOfDeposit > 1000) {
-            finalBalance += amountOfDeposit * 0.01 + amountOfDeposit+initialBalance;
+          if (amountOfDeposit > 1000) {
+            finalBalance = amountOfDeposit / 100 + amountOfDeposit+initialBalance;
         } else {
-            finalBalance += amountOfDeposit+initialBalance;
+            finalBalance = amountOfDeposit+initialBalance;
         }
     }
 
-
-    public static void main(String[] args){
-        deal(20000);
-
-    }
-}
